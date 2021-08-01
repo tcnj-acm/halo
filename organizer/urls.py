afcc5c7335a41c101169a464f2ee6bc2a08a7e2b
Abhi Vempati <abhijitvempati@gmail.com>
2021-08-01 15:17:14 -0400

    listing and searching through all hackers

--- organizer/urls.py
+++ organizer/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, re_path
-from organizer import views 
+from organizer import views
 
 urlpatterns = [
     path('odash', views.dash, name='organizer-dash'),
+    path('hackers', views.all_hackers, name='all-hackers'),
 ]
--- organizer/views.py
+++ organizer/views.py
@@ -1,7 +1,29 @@
 from django.shortcuts import render
-
+from hacker.models import hacker
+from django.db.models import Q
 # Create your views here.
 
+
 def dash(request):
+
+    # If the user is head organizer
+    # Display card to access django admin
+    # Display card to access page of other organizers
+    # is_head_organizer = user.groups.filter(name='head-organizer')
     context = {}
     return render(request, 'organizers/dashboard.html', context)
+
+
+def all_hackers(request):
+
+    url_parameter = request.GET.get("q")
+    if url_parameter:
+        hackers_all = hacker.objects.filter(
+            Q(hacker__first_name__icontains=url_parameter) | Q(hacker__last_name__icontains=url_parameter) |
+            Q(hacker__email__icontains=url_parameter)
+        )
+    else:
+        hackers_all = hacker.objects.all()
+
+    context = {'hackers_all': hackers_all}
+    return render(request, 'organizers/hackersdisplay.html', context)
