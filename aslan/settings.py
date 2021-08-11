cb474eafe62f3a6ffec815282dc610b2d98efd22
Abhi Vempati <abhijitvempati@gmail.com>
2021-08-10 20:32:49 -0400

    added hosts stuff. need a better way to allow dev and prod hosts

--- aslan/settings.py
+++ aslan/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+3f=4(3ybcnw!5zhxkvbyzxqp(4vlq1jb!w&cb69yhbc!8u-_k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
