353c46fd338181c0b0a8683a59897be90f48938d
sterly <deracys1@tcnj.edu>
2021-10-04 21:32:07 -0400

    Remove redundant files, update sponsors and email

--- default/templates/defaults/mail.php
+++ default/templates/defaults/mail.php
@@ -2,8 +2,7 @@
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-        # FIX: Replace this email with recipient email
-        $mail_to = "USEREMAIL@gmail.com";
+        $mail_to = "acm@tcnj.edu.com";
         
         # Sender Data
         $subject = trim($_POST["subject"]);
