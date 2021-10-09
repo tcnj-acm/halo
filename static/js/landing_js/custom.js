54f0c5e60abe0230ff1169466bf9c2239d821ba3
sterly <deracys1@tcnj.edu>
2021-10-08 21:52:24 -0400

    Remove commented code, remove calls to removed plugins

--- static/js/landing_js/custom.js
+++ static/js/landing_js/custom.js
@@ -66,28 +66,4 @@
         offset: 195
     });
 
-
-
-
-
-
-          /*START GOOGLE MAP*/
-          function initialize() {
-            var mapOptions = {
-              zoom: 15,
-              scrollwheel: false,
-              center: new google.maps.LatLng(40.7127837, -74.00594130000002)
-            };
-            var map = new google.maps.Map(document.getElementById('map'),
-                mapOptions);
-            var marker = new google.maps.Marker({
-              position: map.getCenter(),
-              icon: 'assets/img/map_pin.png',
-              map: map
-            });
-          }
-          google.maps.event.addDomListener(window, 'load', initialize);	
-          /*END GOOGLE MAP*/	
-
-
 })(window.jQuery);
