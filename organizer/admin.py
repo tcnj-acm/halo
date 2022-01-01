c2de85b476230b227cd24627186f39f04c1a75e9
Kevin <willik39@tcnj.edu>
2022-01-01 13:23:23 -0500

    added new models for wiatlist feature. Updated str functions for exicting models when viewed in admin page

--- organizer/admin.py
+++ organizer/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import organizer
+from .models import organizer, WaitingListControl, WaitingListSupervisors
 # Register your models here.
 admin.site.register(organizer)
+admin.site.register(WaitingListControl)
+admin.site.register(WaitingListSupervisors)
