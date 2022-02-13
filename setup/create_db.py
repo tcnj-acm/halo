48fb312334a17159d6709654da170fe15c40a13c
Abhi Vempati <abhijitvempati@gmail.com>
2022-02-13 02:17:44 -0500

    added volunteers to the setup script

--- setup/create_db.py
+++ setup/create_db.py
@@ -12,8 +12,8 @@ from default.helper import add_group
 
 # total count (make less than 30 for each for now)
 TOTAL_ORGANIZERS = 5
+TOTAL_VOLUNTEERS = 3
 TOTAL_HACKERS = 10
-
 
 first_name_organizers = [
     'Abhi',
@@ -45,6 +45,32 @@ address_organizers = [
     '5 Bellflower Ct',
     '6 Cranberry Ct'
 ]
+
+###################################
+first_name_volunteers = [
+    'Sahi',
+    'David',
+    'Dom',
+
+]
+
+last_name_volunteers = [
+    'Reganta',
+    'Greco',
+    'Lamastra',
+]
+
+email_volunteers = [
+    'sahi@aslan.com',
+    'david@aslan.com',
+    'dom@aslan.com',
+]
+address_volunteers = [
+    '234 Union St',
+    '25 Warren Ave',
+    '100 Campus Town',
+]
+
 
 ###################################
 ###################################
@@ -201,6 +227,13 @@ def create_users():
         new_user.set_password('hacker123!')
         new_user.save()
 
+    for i in range(TOTAL_VOLUNTEERS):
+        new_user = CustomUser.objects.create(
+            email=email_volunteers[i], first_name=first_name_volunteers[
+                i], last_name=last_name_volunteers[i], address=address_volunteers[i]
+        )
+        new_user.set_password('hacker123!')
+        new_user.save()
     for i in range(TOTAL_HACKERS):
         new_user = CustomUser.objects.create(email=email_hackers[i], first_name=first_name_hackers[i], last_name=last_name_hackers[i],
                                              address=address_hackers[i], food_preference=food_choices_hackers[i], shirt_size=shirt_sizes_hackers[i])
@@ -212,6 +245,13 @@ def create_organizers():
 
     for i in range(TOTAL_ORGANIZERS):
         new_user = CustomUser.objects.get(email=email_organizers[i])
+        new_org = OrganizerInfo.objects.create(user=new_user)
+        add_group(new_user, 'organizer')
+
+
+def create_volunteers():
+    for i in range(TOTAL_VOLUNTEERS):
+        new_user = CustomUser.objects.get(email=email_volunteers[i])
         new_org = OrganizerInfo.objects.create(user=new_user)
         add_group(new_user, 'organizer')
 
@@ -258,6 +298,14 @@ def add_organizers_to_features():
         org_perm.permission.add(permissions_list[0], permissions_list[1],
                                 permissions_list[2], permissions_list[3], permissions_list[4])
 
+    for i in range(TOTAL_VOLUNTEERS):
+        user = CustomUser.objects.get(email=email_organizers[i])
+        org = OrganizerInfo.objects.get(user=user)
+
+        org_perm = OrganizerPermission.objects.create(organizer=org)
+        org_perm.permission.add(permissions_list[1],
+                                permissions_list[2], permissions_list[3])
+
 
 # Driver code
 # site startup code
@@ -265,6 +313,7 @@ create_groups()
 create_super_user()
 create_users()
 create_organizers()
+create_volunteers()
 create_hackers()
 
 # admin startup code
