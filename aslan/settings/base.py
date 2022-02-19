e9c2466220c4abfdc400bf7adb7be8f2f9b0a1a5
Abhi Vempati <abhijitvempati@gmail.com>
2022-02-19 02:32:44 -0500

    updated prod settings. collected static to aws

--- aslan/settings/base.py
+++ aslan/settings/base.py
@@ -140,7 +140,7 @@ WAITLIST_EXEMPT_URLS = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/images/'
--- aslan/settings/prod.py
+++ aslan/settings/prod.py
@@ -5,7 +5,7 @@ import dj_database_url
 import os 
 
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
-DEBUG = True
+DEBUG = False
 
 
 # DATABASES = {
@@ -41,12 +41,13 @@ EMAIL_HOST_PASSWORD = os.getenv('EM_HOST_PASSWORD')
 EMAIL_USE_TLS=os.getenv('EM_USE_TLS')
 
 # AWS S3
+
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_FILE_OVERWRITE = os.getenv('AWS_S3_FILE_OVERWRITE')
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 django_heroku.settings(locals(), test_runner=False)
