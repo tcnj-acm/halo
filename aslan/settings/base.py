c76955910fdaf4d38e079f0b05b9d4ae2ff3e064
Abhi Vempati <abhijitvempati@gmail.com>
2022-02-19 13:31:49 -0500

    allowed hosts modification

--- aslan/settings/base.py
+++ aslan/settings/base.py
@@ -18,7 +18,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".herokuapp.com", "hacktcnj.com"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".herokuapp.com", ".hacktcnj.com"]
 
 EMAIL_OUTGOING='contact@hacktcnj.com'
 
