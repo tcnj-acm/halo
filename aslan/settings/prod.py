388239eaec1a1d004ce492170f70e96907aff409
Abhi Vempati <abhijitvempati@gmail.com>
2022-02-19 12:25:54 -0500

    updated production setting variables

--- aslan/settings/prod.py
+++ aslan/settings/prod.py
@@ -35,17 +35,17 @@ DATABASES['default'].update(db_from_env)
 # Email
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.getenv('EM_HOST')
-EMAIL_PORT = os.getenv('EM_PORT')
+EMAIL_PORT = 587
 EMAIL_HOST_USER = os.getenv('EM_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EM_HOST_PASSWORD')
-EMAIL_USE_TLS=os.getenv('EM_USE_TLS')
+EMAIL_USE_TLS=True
 
 # AWS S3
 
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
-AWS_S3_FILE_OVERWRITE = os.getenv('AWS_S3_FILE_OVERWRITE')
+AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
