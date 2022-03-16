f055ccac7264527a8b44c406b617e4ce0545c430
Abhi Vempati <abhijitvempati@gmail.com>
2022-03-16 11:01:26 -0400

    updated dev settings media url

--- aslan/settings/dev.py
+++ aslan/settings/dev.py
@@ -20,10 +20,10 @@ EM_HOST_PASSWORD = None
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-MEDIA_URL = '/images/'
+MEDIA_URL = '/'
 
 STATICFILES_DIRS = [
     BASE_DIR/'static'
