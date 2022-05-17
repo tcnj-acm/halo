7ebb9dcb29a8ecfde4dd8d1a16d7e34391b7ee29
Kevin Williams <willik39@tcnj.edu>
2022-05-16 20:54:31 -0400

    updates event model test to use exception

--- hacker/tests/test_models.py
+++ hacker/tests/test_models.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from hacker.models import HackerInfo
 from default.models import Event
+from django.core.exceptions import ObjectDoesNotExist
 
 import datetime
 
@@ -19,10 +20,8 @@ class EventModelTest(TestCase):
         self.create_event("Cup Stacking", "Come compete for cool prizes in the cupstacking event!", datetime.time(10,30,0), datetime.time(11,30,0), datetime.date(2021, 4, 9) )
         eventObj = Event.objects.get(title="Cup Stacking")
         eventObj.delete()
-        try:
-            deletedEvent = Event.objects.get(title="Cup Stacking") #calls an exception if not found
-        except:
-            deletedEvent = None
 
-        self.assertEqual(deletedEvent, None)
+        with self.assertRaises(ObjectDoesNotExist):
+            Event.objects.get(title="Cup Stacking") #calls an exception if not found
+
 
