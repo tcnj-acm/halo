50b9b933e19c3a56c29e54540084072b830e0711
Simon Blamo <simonblamo.dev@proton.me>
2023-11-28 18:20:59 -0500

    Changed max connection age

--- halo/settings/prod.py
+++ halo/settings/prod.py
@@ -32,7 +32,7 @@ DATABASES = {
 #     }
 # }
 
-db_from_env = dj_database_url.config(conn_max_age=1000)
+db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 # Email
