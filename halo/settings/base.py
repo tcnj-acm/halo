f18894f653609b1310cff3f797b8c302d5da71e0
Simon Blamo <simonblamo.dev@proton.me>
2023-11-30 09:55:43 -0500

    Attempting to fix server error #7

--- halo/settings/base.py
+++ halo/settings/base.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'teams',
     'storages',
     'phone_field'
-
 ]
 
 
--- halo/settings/prod.py
+++ halo/settings/prod.py
@@ -32,7 +32,8 @@ DATABASES = {
 #     }
 # }
 
-db_from_env = dj_database_url.config(conn_max_age=500)
+# db_from_env = dj_database_url.config(conn_max_age=500)
+db_from_env = dj_database_url.config(conn_max_age=3000)
 DATABASES['default'].update(db_from_env)
 
 # Email
