0a29444306557839a8fd934df02c665fa2ed7106
Simon Blamo <simonblamo.dev@proton.me>
2023-11-30 18:08:12 -0500

    Attempting to fix server error #10

--- halo/settings/prod.py
+++ halo/settings/prod.py
@@ -68,6 +68,3 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
-import django_on_heroku
-django_on_heroku.settings(locals())
