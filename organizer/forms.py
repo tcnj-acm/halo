460b5943fe60f370c91aba888a6794936636538e
Simon-Blamo <simonblamo.dev@proton.me>
2024-09-25 18:44:45 -0400

    Pushing possible fix

--- organizer/forms.py
+++ organizer/forms.py
@@ -18,7 +18,7 @@ class OrganizerCreationForm(forms.Form):
     def clean_email(self):
         email = self.cleaned_data['email'].lower()
 
-        if CustomUser.objects.filter(email=email).exists() and CustomUser.objects.filter(email=email)["is_admin"] == True:
+        if CustomUser.objects.filter(email=email).exists() and CustomUser.objects.filter(email=email).get("is_admin") == True:
             self.add_error('email',"This Email is already in the system!")
         
         return email
